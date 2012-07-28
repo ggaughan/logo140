390b28585458dc58d98e021084173ebff0b3ee54
Greg Gaughan <gjgaughan@googlemail.com>
2012-07-28 16:32:55 +0100

    More lexing and parsing

--- parser.py
+++ parser.py
@@ -1,68 +1,44 @@
-# Logo (cut-down) Parser
-# Leeds Hack 2012
-# Monkey Tennis
+import ply.yacc as yacc
 
-reserved = {
-   'repeat' : 'REPEAT',
-   'fd': 'FORWARD',
-   #etc
-}
+# Get the token map from the lexer.  This is required.
+from lexer import tokens
 
-# List of token names.   This is always required
-tokens = [
-   'NUMBER',
-   'PLUS',
-   'MINUS',
-   'TIMES',
-   'DIVIDE',
-   'LPAREN',
-   'RPAREN',
-   'ID'] + list(reserved.values())
+def p_commands(p):
+    '''commands : command
+                  | commands command
+    '''
+    if len(p) > 2:
+        p[0] = p[1]
+        p[0].append(p[2])
+    else:
+        p[0] = [p[1]]
 
-# Regular expression rules for simple tokens
-#t_PLUS    = r'\+'
-#t_MINUS   = r'-'
-#t_TIMES   = r'\*'
-#t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-#todo better?
-literals = "+-*/"
+def p_command(p):
+    'command : movement'
+    p[0] = p[1]   
+    
+def p_movement(p):
+    'movement : movement_type NUMBER'
+    p[0] = (p[1], p[2])
+    
+def p_movement_type(p):
+    '''movement_type : FORWARD
+                     | BACKWARD
+    '''
+    p[0] = p[1]
 
-# A regular expression rule with some action code
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)    
-    return t
-
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
-
-def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value,'ID')    # Check for reserved words  #todo make case insensitive
-    return t
-
-
-# Error handling rule
-def t_error(t):
-    print "Illegal character '%s'" % t.value[0]
-    t.lexer.skip(1)
+parser = yacc.yacc()
+    
     
 if __name__ == "__main__":
-    import ply.lex as lex
+    # Build the parser
+    parser = yacc.yacc()
 
-    # Build the lexer
-    lexer = lex.lex()
-
-    # Test it out
-    data = '''  repeat  3 + greg + 4 * 10      + -20 *2  IF then ELse '''
-
-    # Give the lexer some input
-    lexer.input(data)
-
-    # Tokenize
     while True:
-        tok = lexer.token()
-        if not tok: break      # No more input
-        print tok
+        try:
+            s = raw_input('calc > ')
+        except EOFError:
+            break
+        if not s: continue
+        result = parser.parse(s)
+        print result    
